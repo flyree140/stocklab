282c4c9ff031efda6437075ffb7fab32298fce25
flyree140 <63763157+flyree140@users.noreply.github.com>
2026-02-28 10:10:44 +0000

    new

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/stocklab/",   // ★ repo 名稱要一致：stocklab
   plugins: [react()],
-})
+});
